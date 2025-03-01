50f74eeef72ce9ade3cfc329d047ef4e4f9d542e
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-01 14:21:09 +0700

    add comment

--- src/libs/runValidation.ts
+++ src/libs/runValidation.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
+// this file is no longer in use
+// this file was previously used for the initial design of the check schema in the middleware file
+
 export default (req: Request, res: Response, next: NextFunction): void => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
