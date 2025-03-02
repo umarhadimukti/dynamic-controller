9c3ab175f29c8c66b481d7c22b716f4f526d255d
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-03 05:57:04 +0700

    draft templates model

---
+++ src/system/templates/model.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import moment from 'moment';
+
+const { Schema, model } = mongoose;
+
+type ISchema = Document;
+
+const tableSchema = new Schema<ISchema>(
+    {
+        // scheme here..
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+            transform: (_, ret) => {
+                ret.createdAt = moment(ret.createdAt).format('YYYY-MM-DD HH:mm:ss.SSS');
+                ret.updatedAt = moment(ret.updatedAt).format('YYYY-MM-DD HH:mm:ss.SSS');
+                return ret;
+            },
+        },
+        toObject: { 
+            virtuals: true,
+            transform: (_, ret) => {
+                ret.createdAt = moment(ret.createdAt).format('YYYY-MM-DD HH:mm:ss.SSS');
+                ret.updatedAt = moment(ret.updatedAt).format('YYYY-MM-DD HH:mm:ss.SSS');
+                return ret;
+            },
+         },
+    }
+);
+
+const TableSchema = model<ISchema>('Schema', tableSchema);
+
+export default TableSchema;
