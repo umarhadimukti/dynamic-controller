4534225ad4dc7f4b8dc6f836784fea8ebedd0f87
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-11 16:21:11 +0700

    fix

--- src/models/LoadModels.ts
+++ src/models/LoadModels.ts
@@ -12,10 +12,4 @@ app.use('/products', dynamicRoute(Product));
 import User from "./User";
 app.use('/users', dynamicRoute(User));
 
-import MfProposal from "./MfProposal";
-app.use('/proposals', dynamicRoute(MfProposal));
-
-import MfProduct from "./MfProduct";
-app.use('/mf-products', dynamicRoute(MfProduct));
-
 export default app;
