524cbf22d563999853f7288cc723abae2e27a261
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-15 04:09:55 +0700

    styling

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import InputValidation from '../libs/InputValidation';
 
 const app: Express = express();
