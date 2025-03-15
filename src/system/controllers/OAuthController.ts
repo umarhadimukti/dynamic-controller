e3b4ada2f1ab57a7b0e5f97f880d2443009bcaa6
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-16 05:17:45 +0700

    draft oauth controller

---
+++ src/system/controllers/OAuthController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+class OAuthController
+{
+    public googleLogin (req: Request, res: Response)
+    {
+
+    }
+
+    public callbackLogin (req: Request, res: Response)
+    {
+        
+    }
+}
+
+export default OAuthController;
--- src/system/routes/system.ts
+++ src/system/routes/system.ts
@@ -3,10 +3,12 @@ import User from "../../models/User";
 import AuthController from "../controllers/AuthController";
 import ModuleController from "../controllers/ModuleController";
 import express, { Request, Response, Express } from "express";
+import OAuthController from "../controllers/OAuthController";
 
 const app: Express = express();
 const moduleController: ModuleController = new ModuleController;
 const authController = new AuthController(User);
+const oAuthController = new OAuthController;
 
 app.post('/system/modules', (req: Request, res: Response): void => {
     moduleController.createModel(req, res);
@@ -18,10 +20,18 @@ app.post('/register', (req: Request, res: Response): void => {
 
 app.post('/login', (req: Request, res: Response): void => {
     authController.login(req, res);
-})
+});
 
 app.post('/refresh-token/:token', (req: Request, res: Response): void => {
     authController.refreshToken(req, res);
-})
+});
+
+app.get('/auth/login', (req: Request, res: Response): void => {
+    oAuthController.googleLogin(req, res);
+});
+
+app.get('/auth/login/callback', (req: Request, res: Response): void => {
+    oAuthController.callbackLogin(req, res);
+});
 
 export default app;
